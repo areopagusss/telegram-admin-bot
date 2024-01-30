c15888531d38b2cc71289b02d434ee901220fd36
Alexander Mordvinkin <Umets_953@mail.ru>
2024-01-30 21:32:51 +0200

    adding_middlewares

---
+++ bot/middlewares/__init__.py
@@ -0,0 +1,3 @@
+from .target_user import TargetUserMiddleware, target_user_required
+from .pyrogram_client import PyrogramClientMiddleware, pyrogram_client_required
+from .skip_update import SkipUpdateMiddleware
